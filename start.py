b7f9c6c2cac68e658122470efa6288a4e28a8168
Serge Sukhanov <ssuhanov@gmail.com>
2020-05-14 21:23:06 +0300

    Add database and store content there

--- start.py
+++ start.py
@@ -1,12 +1,38 @@
-from collections import namedtuple
-
 from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+# for PostgreSQL:
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://user_name:password@host(localhost)/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fl_learn.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+# db.create_all()
 
-Message = namedtuple('Message', 'text tag')
-messages = []
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, text, tags):
+        self.text = text.strip()
+        self.tags = [
+            Tag(text=tag.strip()) for tag in tags.split(',')
+        ]
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 
 @app.route('/', methods=['GET'])
@@ -16,13 +42,15 @@ def hello_world():
 
 @app.route('/main', methods=['GET'])
 def main():
-    return render_template('main.html', messages=messages)
+    return render_template('main.html', messages=Message.query.all())
 
 
 @app.route('/add_message', methods=['POST'])
 def add_message():
     text = request.form['text']
     tag = request.form['tag']
-    messages.append(Message(text, tag))
+
+    db.session.add(Message(text, tag))
+    db.session.commit()
 
     return redirect(url_for('main'))
