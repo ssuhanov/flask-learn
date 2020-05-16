04f8d7813d312ffd764bc2275c390f0d87cb6657
Serge Sukhanov <ssuhanov@gmail.com>
2020-05-16 16:33:30 +0300

    Implement simple auth and registration

--- sweater/__init__.py
+++ sweater/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+app.secret_key = 'some_secret_salt'
 # for PostgreSQL:
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://user_name:password@host(localhost)/db_name'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fl_learn.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 
 from sweater import models, routes
